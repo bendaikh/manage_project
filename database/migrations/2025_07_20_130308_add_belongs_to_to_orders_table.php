2bdd76afd14a0d7bcc8859aa673c804bbfa0e3a1
bendaikh <bendaikh2000@gmail.com>
2025-07-20 12:09:56 -0700

    comited

--- database/migrations/2025_07_20_130308_add_belongs_to_to_orders_table.php
+++ database/migrations/2025_07_20_130308_add_belongs_to_to_orders_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            //
+            $table->enum('belongs_to', ['confirmation', 'delivery'])->default('confirmation')->after('order_status_id');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            //
+            $table->dropColumn('belongs_to');
         });
     }
 };
